98b4ffb49d14b49b4d8eb1030cb9006166b1f73f
Vi Grey <development@pariahvi.com>
2015-01-03 23:39:35 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ from distutils.core import setup
 if sys.version_info < (3, 0, 0):
     print('White Rabbit requires Python 3')
 
-version = '1.0.3.0
+version = '1.0.3.1
 mainscript = 'white-rabbit'
 
 setup(
     name='White Rabbit',
     description='Console-based time management program',
-    author='PariahVi (http://pariahvi.com)',
-    author_email='vi@pariahvi.com',
+    author='Vi Grey (http://pariahvi.com)',
+    author_email='development@pariahvi.com',
     license='BSD',
     version=version,
-    url='https://github.com/PariahVi/white-rabbit',
+    url='https://github.com/PariahVi/WhiteRabbit',
     scripts=[mainscript]
     )
