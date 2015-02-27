01cfa16b187314d2bad5724257ee6c04d66d3b8a
Vi Grey <development@pariahvi.com>
2015-02-27 06:09:08 -0600

    Update setup.py
    Updated setup.py info

--- setup.py
+++ setup.py
@@ -16,4 +16,18 @@ setup(
     version=version,
     url='https://github.com/PariahVi/WhiteRabbit',
     scripts=[mainscript]
-    )
+    platforms='No particular restrictions',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Operating System :: OS Independent',
+        'Topic :: Other/Nonlisted Topic'
+    ]
+)
